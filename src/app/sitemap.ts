e529e76613aa2a66afadb3717cd05cebd2b20c58
Marta Kozma <kozmarti@gmail.com>
2025-10-19 21:02:40 +0200

    meta photo change

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,7 +17,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/#skills`,
+      url: `${baseUrl}/#services`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
